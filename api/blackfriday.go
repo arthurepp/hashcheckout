acd51a495b2dcaa74a432faac991047148185db4
Arthur Epprecht <arthur.epp@gmail.com>
2022-02-13 19:01:17 -0300

    Implement blackfriday

--- api/blackfriday.go
+++ api/blackfriday.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"time"
+)
+
 type BlackFriday interface {
 	AddGiftOnBlackFriday(cResponse CheckoutRespose)
+}
+
+type BlackFridayService struct {
+	BlackFridayDate time.Time
+}
+
+func NewBlackFridayService(date time.Time) BlackFridayService {
+	return BlackFridayService{BlackFridayDate: date}
+}
+
+func (bf *BlackFridayService) AddGiftOnBlackFriday(cResponse *CheckoutRespose) {
+	isBackFriday := isBackFriday(bf.BlackFridayDate)
+	if isBackFriday {
+		cResponse.Products = append(cResponse.Products, ProductResponse{
+			ID:          0,
+			Quantity:    1,
+			UnitAmount:  0,
+			TotalAmount: 0,
+			Discount:    0,
+			IsGift:      true,
+		})
+	}
+}
+
+func isBackFriday(date time.Time) bool {
+	return time.Now().Truncate(time.Hour*24).Format("2006-01-02") == date.Truncate(time.Hour*24).Format("2006-01-02")
 }
---
+++ api/blackfriday_test.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestIsBlackFriday(t *testing.T) {
+	var products []ProductResponse
+	products = append(products, ProductResponse{
+		ID:          1,
+		Quantity:    3,
+		UnitAmount:  200,
+		TotalAmount: 600,
+		Discount:    0,
+		IsGift:      false,
+	})
+	products = append(products, ProductResponse{
+		ID:          2,
+		Quantity:    2,
+		UnitAmount:  100,
+		TotalAmount: 200,
+		Discount:    0,
+		IsGift:      false,
+	})
+	checkout := CheckoutRespose{
+		TotalAmount:             800,
+		TotalAmountWithDiscount: 800,
+		TotalDiscount:           0,
+		Products:                products,
+	}
+
+	x := time.Now().Truncate(time.Hour * 24).Format("2006-01-02")
+	fmt.Println(x)
+
+	date, err := time.Parse("2006-01-02", time.Now().Truncate(time.Hour*24).Format("2006-01-02"))
+	if err != nil {
+		panic(err)
+	}
+	blackFridayService := NewBlackFridayService(date)
+	blackFridayService.AddGiftOnBlackFriday(&checkout)
+
+	if len(checkout.Products) < 3 {
+		t.Errorf("invalid gift")
+	}
+}
+
+func TestNotIsBlackFriday(t *testing.T) {
+	var products []ProductResponse
+	products = append(products, ProductResponse{
+		ID:          1,
+		Quantity:    3,
+		UnitAmount:  200,
+		TotalAmount: 600,
+		Discount:    0,
+		IsGift:      false,
+	})
+	products = append(products, ProductResponse{
+		ID:          2,
+		Quantity:    2,
+		UnitAmount:  100,
+		TotalAmount: 200,
+		Discount:    0,
+		IsGift:      false,
+	})
+	checkout := CheckoutRespose{
+		TotalAmount:             800,
+		TotalAmountWithDiscount: 800,
+		TotalDiscount:           0,
+		Products:                products,
+	}
+
+	date, err := time.Parse("2006-01-02", "2022-02-03")
+	if err != nil {
+		panic(err)
+	}
+	blackFridayService := NewBlackFridayService(date)
+	blackFridayService.AddGiftOnBlackFriday(&checkout)
+
+	if len(checkout.Products) > 2 {
+		t.Errorf("invalid gift")
+	}
+}
